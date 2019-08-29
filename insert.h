4badfb2cbb082f5d217e5755d28559de51e05fb2
Johan Tanta <johan.tanta@utec.edu.pe>
2019-08-28 21:41:28 -0500

    Algorithm insert sort

--- insert.h
+++ insert.h
@@ -8,10 +8,19 @@ class InsertSort : public Sort {
         InsertSort(int *elements, size_t size) : Sort(elements, size) {}
 
         void execute() {
-            // TODO
-        }
 
-        inline string name() { return "InsertSort"; }
+            int temp, indice;
+            for (int i = 1; i < size; ++i) {
+                temp = elements[i];
+                indice = i - 1;
+                while (indice >= 0 && elements[indice] > temp) {
+                    elements[indice + 1] = elements[indice];
+                    indice--;
+                }
+                elements[indice + 1] = temp;
+            }
+        }
+        inline string name(){ return "InsertSort"; }
 };
 
 #endif
