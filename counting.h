b5e4371ffb2653f06daed022e564e79e328133f5
Johan Tanta <johan.tanta@utec.edu.pe>
2019-09-01 23:15:11 -0500

    Algorithm couting sort

--- counting.h
+++ counting.h
@@ -9,7 +9,31 @@ class CountingSort : public Sort {
         CountingSort(int *elements, size_t size) : Sort(elements, size) {}
 
         void execute() {
-            // TODO
+            int* listaOrdenada = new int[size]{0};
+            int min = *min_element(elements,elements+size);
+            int max = *max_element(elements,elements+size);
+            int length = max - min + 1;
+            int* count = new int[length]{0};
+
+            for (int i = 0; i < size; ++i) {
+                count[elements[i]-min]++;
+            }
+
+            for (int i = 1; i < length; ++i) {
+                count[i]+=count[i-1];
+            }
+
+            for (int i = size - 1; i >= 0; --i) {
+                listaOrdenada[count[elements[i] - min] - 1] = elements[i];
+                count[elements[i] - min]--;
+            }
+
+            for (int i = 0; i < size; ++i) {
+                elements[i] = listaOrdenada[i];
+            }
+
+            delete[] count;
+            delete[] listaOrdenada;
         }
 
         inline string name() { return "CountingSort"; }
