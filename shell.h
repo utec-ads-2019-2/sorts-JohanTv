513838894803167f72273ba87b18147a05cb393b
Johan Tanta <johan.tanta@utec.edu.pe>
2019-09-04 00:09:43 -0500

    Shell Sort

--- shell.h
+++ shell.h
@@ -8,7 +8,18 @@ class ShellSort : public Sort {
         ShellSort(int *elements, size_t size) : Sort(elements, size) {}
 
         void execute() {
-            // TODO
+            int j;
+            int temp;
+            for (int gap = size/2; gap > 0; gap /= 2){
+                for (int i = gap; i < size; i += 1){
+                    temp = elements[i];
+
+                    for (j = i; j >= gap && elements[j - gap] > temp; j -= gap){
+                        elements[j] = elements[j - gap];
+                    }
+                    elements[j] = temp;
+                }
+            }
         }
 
         inline string name() { return "ShellSort"; }
